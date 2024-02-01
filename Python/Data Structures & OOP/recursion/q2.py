e367bf4eb87bfd0fa6fa25e60221d1b78f63b865
awanh2 <awanh2@L129-25.winlabs.computing.dcu.ie>
2024-02-01 13:34:16 +0000

    update

--- Python/Data Structures & OOP/recursion/q2.py
+++
@@ -1,9 +0,0 @@
-# Q2
-def reverse_integer(n):
-    if n == 0:
-        return ""
-    else:
-        return str(n % 10) + str(reverse_integer(n // 10))
-
-print(reverse_integer(123))
-print(reverse_integer(456))
--- Python/Data Structures & OOP/review_&_oop/q5.py
+++
@@ -1,7 +0,0 @@
-def histogram(list, shape):
-    i = 0
-    while i < len(list):
-        print(list[i] * shape)
-        i = i + 1
-   
-histogram([6, 2, 15, 3, 20, 5], "=")
